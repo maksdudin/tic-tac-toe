4d8068df760e504f195e656e0238a78efba03b8b
vasylmalik <vasyl.malik@javarush.ru>
2022-06-28 16:46:25 +0300

    Корочее... всё по https://javarush.com/quests/lectures/jru.module3.lecture03 в начальную страницу index.jsp добавили таблицу которая отрисовывается согласно файлс настроек CSS потом добавили возможность кливать по ячейкам,  - за отслеживание кликов отвечает созданный сервлет LogicServlet затем добавили сохранение состояния полей в игре  в открытой сессии за это отвечает добавленный сервлет InitServlet и изменили стартовый путь сервера так что бы он при старте запускал InitServlet

---
+++ src/main/java/com/tictactoe/InitServlet.java
@@ -0,0 +1,39 @@
+package com.tictactoe;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+//сессия будет храниться на сервере, а клиент получит идентификатор сессии в куке с именем “JSESSIONID”.
+// Но сессию не нужно создавать каждый раз, а только в начале игры.
+@WebServlet(name = "InitServlet",value = "/start")
+public class InitServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        //создание новой сессии
+        HttpSession curenSesion = request.getSession(true);
+        Field field = new Field();
+        Map<Integer,Sign> fileData = field.getField();
+
+        //получение списка значений полей
+        List<Sign> data = field.getFieldData();
+
+        // добавление в сесию параметров поля()нужно будет для хранения между запросами
+        curenSesion.setAttribute("field",field);
+        // добавление в сесию значений поля отсортированный по индексу (сортируется в getFieldData()) для  отрисовки
+        // крестиков и ноликов
+        curenSesion.setAttribute("data",data);
+
+        //перенапрвление запроса   на страницу index.jsp через сервер
+         getServletContext().getRequestDispatcher("/index.jsp").forward(request,response);
+
+         // стартовую страницу в томкете нужно сменить на http://localhost:8080/start
+
+    }
+}
